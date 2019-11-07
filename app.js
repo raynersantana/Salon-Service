6354d5a867a6b9a764ce40367a49fd3ea0c7a1dd
rayner <raynersan@hotmail.com>
2019-11-07 11:52:02 -0300

    terminando o cadastro e alguma funcionalidade básica

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const ejs = require('ejs');
 const firebase = require('firebase');
 const Auth = require('./firebase.js');
+const ejs = require('ejs');
 
 let userLogged;
-const app = express();
-var publicDir = require('path').join(__dirname, '/public');
+const app = express()
+var publicDir = require('path').join(__dirname,'/public');
 
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(publicDir));
-app.use('/', express.static(__dirname + '/www'));
-app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
-app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
-// app.use('/style', express.static(__dirname + '/style/'));
+app.use('/', express.static(__dirname + '/www')); // redirect root
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/style', express.static(__dirname + '/style/')); // redirect CSS bootstrap
 app.set('view engine', 'ejs');
 
 firebase.auth().onAuthStateChanged((user) => {
     if(user){
         userLogged = user
-    }else{
+    } else {
         userLogged = null
     }
 })
@@ -30,20 +31,15 @@ app.get('/', (req, res) => {
     res.render('index');
 })
 
-app.post('/cadastro', (req, res) => {
-    Auth.SignUpWithEmailAndPassword(req.body.email, req.body.password)
-    .then((user) => {
-        if(!user.err){
-            let userData = JSON.parse(user)
-            userData = userData.user
-            Auth.insertUserData(userData).then(() => {
-                res.redirect('/dashboard')
-            })
-        }else{
-            return user.err
-        }
-    })
-})
+app.post('/createuser', (req, res) => {
+    Auth.SignUpWithEmailAndPassword(req.body.email,req.body.password).then((user) => {
+       if(!user.err){
+        res.redirect('/dashboard')
+       }else{
+          return user.err
+       }
+   })
+  })
 
 app.post('/login', (req, res) => {
     let getBody = req.body;
@@ -57,12 +53,21 @@ app.post('/login', (req, res) => {
     })
 })
 
+app.post('/input', (req, res) => {
+    let {name} = req.body
+    Auth.InputData(name).then(() => {
+        res.render('dashboard')
+    })
+})
+
 app.get('/dashboard', function(req, res){
     if(userLogged){
-        res.render('dashboard', {user: user, snapshot: snapshot});
+        Auth.GetData().then((data) => {
+            res.render('dashboard', {data});
+        })
     }else{
         res.redirect('/')
     }
-})
+});
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000)
